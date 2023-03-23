be2419c88df153d46f9cbc0c8810c4907bb8d181
송성빈 <aktm1004@naver.com>
2023-03-23 22:23:59 +0900

    setting

---
+++ src/test/java/com/example/restservice/GreetingTest.java
@@ -0,0 +1,36 @@
+package com.example.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingTest {
+
+    @Autowired MockMvc mockMvc;
+
+    @Test
+    void greeting() throws Exception{
+        mockMvc.perform(get("/greeting"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    void greetingWithName() throws Exception{
+        mockMvc.perform(get("/greeting").param("name", "Spring"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring!"));
+    }
+
+
+}
